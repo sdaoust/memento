683e3e849671a7aa37a9b1f6a9d3ae7c7a597cfd
Stephanie Daoust <stephanie.l.daoust@gmail.com>
2017-04-23 22:47:04 -0400

    Initial commit.

---
+++ puzzlesAndDragons/puzzlesAndDragons.js
@@ -0,0 +1,45 @@
+function Monster(monsterEntry) {
+	this.monsterEntry = monsterEntry;
+}
+
+Monster.prototype.getPrimaryElement = function() {
+	if (!this.primaryElement) {
+		this.primaryElement = 
+			this.monsterEntry.field("Primary Element");
+	}
+	return this.primaryElement;
+}
+
+Monster.prototype.getSecondaryElement = function() {
+	if (!this.secondaryElement) {
+		this.secondaryElement = 
+			this.monsterEntry.field("Secondary Element");
+	}
+	return this.primaryElement;
+}
+
+function Drop(dropEntry) {
+	this.dropEntry = dropEntry;
+}
+
+Drop.prototype.getMonster = function() {
+	if (!this.monster) {
+		this.monster = 
+			new Monster(this.dropEntry.field("Monster")[0]);
+	}
+	return this.monster;
+}
+
+Drop.prototype.setPrimaryElement = function(primaryElement) {
+	this.dropEntry.set("Primary Element", primaryElement);
+}
+
+Drop.prototype.setSecondaryElement = function(secondaryElement) {
+	this.dropEntry.set("Secondary Element", secondaryElement);
+}
+
+Drop.prototype.updateElements = function() {
+	var monster = this.getMonster();
+	this.setPrimaryElement(monster.getPrimaryElement());
+	this.setSecondaryElement(monster.getSecondaryElement());
+}
