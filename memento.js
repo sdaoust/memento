1b1184c4bbbaf4fd50fac6ad60bb6e3673783ccb
Stephanie Daoust <stephanie.l.daoust@gmail.com>
2017-05-20 12:14:45 -0400

    entries recalculate when a field changes.

--- memento.js
+++ memento.js
@@ -284,6 +284,9 @@ Data.prototype = (function () {
 		}
 
 		this._entry.set(field.name, fieldValue);
+
+		this._entry.recalc();
+		
 		return this._get({
 			name		 : field.name,
 			DataType	 : field.DataType,
